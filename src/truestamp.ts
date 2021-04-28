b67cec4af7eb3af2f4623bf868a281a6cfba9101
Glenn Rempe <glenn@truestamp.com>
2021-04-28 13:16:22 -0400

    Update to latest truestamp-js lib
    * accessToken vs apiKey
    * Base URI lookup now resides in the lib

--- src/truestamp.ts
+++ src/truestamp.ts
@@ -1,26 +1,10 @@
 import { getAccessTokenWithPrompts, Truestamp } from "./deps.ts"
 
-function getApiBaseUriForEnv(env: string): string {
-  switch (env) {
-    case "development":
-      return "https://dev-api.truestamp.com/v1/"
-
-    case "staging":
-      return "https://staging-api.truestamp.com/v1/"
-
-    case "production":
-      return "https://api.truestamp.com/v1/"
-
-    default:
-      throw new Error(`invalid environment : '${env}'`)
-  }
-}
-
-export async function createTruestampClient(env: string) {
+export async function createTruestampClient(apiEnv: string) {
   // FIXME : https://stackoverflow.com/questions/43623461/new-expression-whose-target-lacks-a-construct-signature-in-typescript
   const client = new (Truestamp as any)({
-    apiKey: await getAccessTokenWithPrompts(env),
-    apiBaseUrl: getApiBaseUriForEnv(env),
+    accessToken: await getAccessTokenWithPrompts(apiEnv),
+    apiEnv: apiEnv,
   })
 
   return client
