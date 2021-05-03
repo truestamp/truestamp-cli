aac87bd094f327705509e767b1173496ed698ef1
Glenn Rempe <glenn@truestamp.com>
2021-05-03 16:22:26 -0400

    focus depsbot on a path

--- src/deps.ts
+++ src/deps.ts
@@ -1,10 +1,12 @@
 import configDir from "https://deno.land/x/config_dir/mod.ts"
 import { sleep } from "https://deno.land/x/sleep/mod.ts"
+import loadJsonFile from "https://deno.land/x/load_json_file@v1.0.0/mod.ts"
+import { decode, validate } from "https://deno.land/x/djwt@v2.2/mod.ts"
+
 import { colors } from "https://deno.land/x/cliffy@v0.18.2/ansi/colors.ts"
 import { Command } from "https://deno.land/x/cliffy@v0.18.2/command/mod.ts"
 import { HelpCommand } from "https://deno.land/x/cliffy@v0.18.2/command/help/mod.ts"
 import { CompletionsCommand } from "https://deno.land/x/cliffy@v0.18.2/command/completions/mod.ts"
-import loadJsonFile from "https://deno.land/x/load_json_file@v1.0.0/mod.ts"
 
 import {
   deleteSavedTokens,
@@ -14,12 +16,7 @@ import {
   getSavedIdTokenPayload,
 } from "./auth.ts"
 
-// depsbot-ignore
-import { decode, validate } from "https://deno.land/x/djwt@v2.2/mod.ts"
-
-// depsbot-ignore
 import Truestamp from "https://cdn.skypack.dev/@truestamp/truestamp-js?dts"
-
 import { createTruestampClient } from "./truestamp.ts"
 
 export {
